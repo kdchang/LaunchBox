4f936db0af49c245aab65d47ace1f3dfdde7734e
kdchang <kdchang.cc@gmail.com>
2014-10-17 20:45:18 +0800

    update icon in admin

--- login.php
+++ login.php
@@ -15,6 +15,7 @@ if ($logined) {
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/jumbotron-narrow.css">
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="shortcut icon" href="../assets/images/icon_128.ico" />
 </head>
 
 <body>
